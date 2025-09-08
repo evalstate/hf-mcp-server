004d13cdbd863089fd46574f09a55b4d5acb5abe
evalstate <1936278+evalstate@users.noreply.github.com>
2025-09-08 17:33:09 +0100

    update clean repo script

--- packages/e2e-python/clean_repo.sh
+++ packages/e2e-python/clean_repo.sh
@@ -1,3 +1,4 @@
 hf repo-files delete --repo-type dataset hf-mcp-server/test-mcp-logs logs/**/
 hf repo-files delete --repo-type dataset hf-mcp-server/test-mcp-logs queries/**/
+hf repo-files delete --repo-type dataset hf-mcp-server/test-mcp-logs sessions/**/
 
